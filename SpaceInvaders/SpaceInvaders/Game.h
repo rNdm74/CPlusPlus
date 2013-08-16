e48aa3dadf2585bd2794a692a809cbeb4784ac7d
rndm <rndm@windowslive.com>
2013-08-16 14:48:19 +1200

    Adding to Repo

---
+++ SpaceInvaders/SpaceInvaders/Game.h
@@ -0,0 +1,98 @@
+#pragma once
+
+
+namespace SpaceInvaders {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Game
+	///
+	/// WARNING: If you change the name of this class, you will need to change the
+	///          'Resource File Name' property for the managed resource compiler tool
+	///          associated with all .resx files this class depends on.  Otherwise,
+	///          the designers will not be able to interact properly with localized
+	///          resources associated with this form.
+	/// </summary>
+	public ref class Game : public System::Windows::Forms::Form
+	{
+	public:
+		Game(void)
+		{
+			InitializeComponent();
+			dbBitmap = nullptr;
+			dbGraphics = nullptr;
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Game()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		Graphics^ dbGraphics;
+		Bitmap^ dbBitmap;
+
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+		this->SuspendLayout();
+		// 
+		// Game
+		// 
+		this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+		this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+		this->BackColor = System::Drawing::Color::Black;
+		this->ClientSize = System::Drawing::Size(624, 442);
+		this->DoubleBuffered = true;
+		this->Name = L"Game";
+		this->ShowIcon = false;
+		this->Text = L"Space Invaders";
+		this->Load += gcnew System::EventHandler(this, &Game::Game_Load);
+		this->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Game::Game_Paint);
+		this->ResumeLayout(false);
+
+			}
+#pragma endregion
+	private: System::Void Game_Paint(System::Object^  sender, System::Windows::Forms::PaintEventArgs^  e) {
+					// Refresh screen
+					Invalidate();
+
+					// Clear background
+					dbGraphics->Clear(BackColor);
+					
+					// Update game	
+
+					// Render game
+					
+					// Make buffer visible
+					e->Graphics->DrawImage(dbBitmap, 0, 0);
+				 }
+	private: System::Void Game_Load(System::Object^  sender, System::EventArgs^  e) {
+					// Create graphics size of the screen
+					dbBitmap = gcnew Bitmap(ClientRectangle.Width, ClientRectangle.Height);
+
+					// Grab its Graphics
+					dbGraphics = Graphics::FromImage(dbBitmap);
+				 }
+		};
+}
+
